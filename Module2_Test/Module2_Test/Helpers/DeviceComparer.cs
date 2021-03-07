0548c4cdd25e6417cc17ad80e0468d7b6b83085a
Vira Vostrikova <vvostrikova@csod.com>
2021-03-07 21:47:13 +0200

    Helpers added

---
+++ Module2_Test/Module2_Test/Helpers/DeviceComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Module2_Test.Models.Entities;
+
+namespace Module2_Test.Helpers
+{
+    public class DeviceComparer : IComparer
+    {
+        public int Compare(object i1, object i2)
+        {
+            var a = i1 as HomeElectricDevice;
+            var b = i2 as HomeElectricDevice;
+
+            if (a.DateOfManufacture > b.DateOfManufacture)
+            {
+                return -1;
+            }
+            else if (a.DateOfManufacture < b.DateOfManufacture)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Module2_Test/Module2_Test/Helpers/SmartHomeExtension.cs
@@ -0,0 +1,43 @@
+﻿using Module2_Test.Enums;
+using Module2_Test.Models;
+using Module2_Test.Models.Entities;
+
+namespace Module2_Test.Helpers
+{
+    public static class SmartHomeExtension
+    {
+        public static HomeElectricDevice[] FilterProducerOrigin(this HomeElectricDevice[] devices, Producers producer, Countries country)
+        {
+            var filteredItems = new HomeElectricDevice[devices.Length];
+            var count = 0;
+
+            for (var i = 0; i < devices.Length; i++)
+            {
+                if (devices[i].ProducerName == producer && devices[i].CountryProducer == country)
+                {
+                    filteredItems[i] = devices[i];
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                return null;
+            }
+
+            var result = new HomeElectricDevice[count];
+            var counter = 0;
+
+            for (var i = 0; i < filteredItems.Length; i++)
+            {
+                if (filteredItems[i] != null)
+                {
+                    result[counter] = filteredItems[i];
+                    counter++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
